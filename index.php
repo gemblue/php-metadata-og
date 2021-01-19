33a70a03c062c96af877651a0476de2bd5fdadb5
Ahmad Oriza <ahmadoriza@gmail.com>
2021-01-19 11:06:48 +0700

    Init

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=<device-width>, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Apa Itu Preact? - Codepolitan</title>
+    <link rel="canonical" href="https://www.codepolitan.com/">
+    <meta property="og:locale" content="id_ID">
+    <meta property="og:type" content="article">
+    <meta property="og:title" content="Apa Itu Preact? - Codepolitan">
+    <meta property="og:description" content="Dalam beberapa tahun terakhir, React tetap menjadi library yang paling sering digunakan untuk membangun single-page application">
+    <meta property="og:url" content="https://www.codepolitan.com/apa-itu-preact">
+    <meta property="og:site_name" content="CodePolitan.com">
+    <meta property="article:publisher" content="https://www.instagram.com/codepolitan">
+    <meta property="og:image" content="https://i.ibb.co/5hZZ9vq/Apakah-kamu-masih-menggunakan-lodash.jpg">
+    <meta property="og:image:width" content="700">
+    <meta property="og:image:height" content="350">
+    <link rel="alternate" type="application/rss+xml" title="Apa Itu Preact? - Codepolitan" href="https://www.codepolitan.com/">
+    <meta name="description" content="Dalam beberapa tahun terakhir, React tetap menjadi library yang paling sering digunakan untuk membangun single-page application">
+    <meta property="description" content="Dalam beberapa tahun terakhir, React tetap menjadi library yang paling sering digunakan untuk membangun single-page application">
+    <meta property="language" content="Indonesia">
+    <meta property="revisit-after" content="7">
+    <meta property="rating" content="general">
+    <meta name="google-site-verification" content="ErdTzB1AquCqq7TiWcxi1FiXIdYxCOd-wkx66iJQ72c" />
+    <meta property="webcrawlers" content="all">
+    <meta property="spiders" content="all">
+    <meta property="robots" content="all">
+</head>
+<body>
+    Metadata OG Test.
+</body>
+</html>
